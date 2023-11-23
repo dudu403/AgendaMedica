23a9e73a558a9c91bd1a03e952fa801f4bde7ae5
Dudu403 <eduardomoreira300@outlook.com>
2023-11-23 03:04:53 -0300

    Implementando modulo compartilhado infra

---
+++ AgendaMedica.Infra.Orm/Compartilhado/AgendaMedicaDbContext.cs
@@ -0,0 +1,23 @@
+﻿using AgendaMedica.Dominio.ModuloAtividade;
+using AgendaMedica.Dominio.ModuloMedico;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaMedica.Infra.Orm.Compartilhado
+{
+    public class AgendaMedicaDbContext : DbContext
+    {
+
+        public AgendaMedicaDbContext(DbContextOptions<AgendaMedicaDbContext> options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Medico>();
+
+            modelBuilder.Entity<Atividade>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
